46a1556db637917648df64277b7818139ee4efb3
kfkudjoe <kfkudjoe@gmail.com>
2025-07-13 10:59:55 +0000

    Eighth commit on the video-storage-local branch.
    Created a backup copy of deploy.sh.
    Named it deploy-image.v0.sh
    Created a new update of deploy.sh. Added functionality to create a network named fluxtube-network to facilitate connection with video-streaming and rabbit-mq services.
    Video storage microservice will be accessible on host port 3001.
    Note: rabbitmq service deployment script must be updated to add it to the fluxtube-network docker container network.
    Note: video-streaming-fluxtube service deployment script must be updated to add it to the fluxtube-network docker container network.

--- video-storage-local/deploy-image.sh
+++ video-storage-local/deploy-image.sh
@@ -1,1 +1,20 @@
-docker run -p 3000:3000 -e PORT=3000 video-storage-local-fluxtube
+#!/bin/bash
+
+# Define the common network name
+NETWORK_NAME="fluxtube-network"
+
+# Ensure the network exists. If it already exits, just report so.
+echo "Ensuring Docker network '$NETWORK_NAME' exits..."
+docker network create $NETWORK_NAME || true
+
+# Deploy the video-storage-local-fluxtube microservice; Connect to the 'fluxtube-network'
+echo "Deploying 'video-storage-local-fluxtube' container..."
+docker run -d \
+  --name video-storage-local-fluxtube \
+  --network $NETWORK_NAME \
+  -p 3001:3000 \
+  -e PORT=3000 \
+  video-storage-local-fluxtube
+
+echo "Video Storage microservice deployed."
+echo "Access on host port 3001."
