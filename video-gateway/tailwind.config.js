ce29caf624bbdd6ab1eb8556bd7e9a7d8c609b4b
kfkudjoe <kfkudjoe@gmail.com>
2025-07-19 12:32:02 +0000

    Third commit on the video-frontend branch.
    Configured the tailwindcss app.
    Created the source file for the tailwindcss app.
    Added the build scripts for the sourcefiles of the tailwindcss app.

--- video-gateway/tailwind.config.js
+++ video-gateway/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // Scan all Handlebars files in the 'src/views' directory
+    "./src/views/**/*.hbs",
+    // If there are any inline JS that adds classes, or other HTML files in 'public'
+    "./public/**/*.html", // Include if plan to have static HTM files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
